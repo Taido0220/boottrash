7bf99fcf6d03f1e6ce52357518dec411a649a5ab
Taido0220 <91017652+Taido0220@users.noreply.github.com>
2021-09-26 10:18:47 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,230 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+   
+    <title>Restaurant bootstrap</title>
+    <link rel ="stylesheet" href="css/style03.css">
+  </head>
+  <body>
+    <header>
+         <div class="container-fluid">
+             
+             <div class="row">
+                 <div class="col-md-6 ">
+
+                 <img src ="Images/logo.png" alt=""class="img-responsive">
+               </div>
+             <div class="col-md-6 ">
+             <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <div class="navbar">
+  <a class="navbar-brand" href="#">Home</a>
+    <a class="navbar-brand" href="#">Catagories</a>
+    <a class="navbar-brand" href="#">Food</a>
+    <a class="navbar-brand" href="#">Contact</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+   </div>
+    
+    </div>
+  </div>
+</nav>
+               
+            </div>
+        <!-- end -->
+
+      
+    
+             </div>
+         <div>
+         </div>
+    <!-- end -->
+    
+    <div class="container-fluid">
+             
+             <div class="row">
+                 <div class="col-md-12 bg-cuatoi">
+
+                
+                 <section class="Food-Search text-center">
+     
+     
+  <div class="container-fluid">
+    <form class="d-flex bg-toi">
+      <input class="form-control me-2" type="search " placeholder="Search for food" aria-label="Search">
+      <button class="btn" type="submit">Search </button>
+    </form>
+</section>
+  </div>
+
+      
+ 
+       </div>
+             </div>
+ 
+    </div>
+</header>
+
+<!-- end -->
+<div class="Categories">
+  <div class="container">
+     <div class="row">
+           <h2 class="text-center"> Explore Food</h2>
+           <div class="col-md-4 ">
+           
+            <img src="Images/pizza.jpg" class="rounded" alt="">
+            <h3 class="text-dark ">Pizza</h3>
+           </div> 
+            <div class="col-md-4 ">
+           
+            <img src="Images/burger.jpg" class="rounded" alt="">
+            <h3 class="text-dark ">Burger</h3>
+           </div> 
+           <div class="col-md-4 ">
+           
+            <img src="Images/momo.jpg" class="rounded" alt="">
+            <h3 class="text-dark ">Momo</h3>
+           </div> 
+      </div>    
+ 
+</div>
+<!-- end -->
+<div class="Food-menu">
+  <div class="container-fluid">
+     <div class="row">
+     <h2 class="text-center"> Food Menu</h2>
+
+
+     <div class="box">
+  <div class="row g-2">
+    <div class="col-6">
+      <div class="p-3 border bg-light">
+      <div class="food-menu-img">
+                 <img src ="Images/menu-pizza.jpg" alt="Pizza chicken"class="img-responsive img-curve">
+               </div>
+               <div class="food-menu-desc">
+                  <h4>Mixed Pizza</h4>
+                  <p class="food-price" >$10.00</p>
+                  <p class="food-detail">
+                      pizza with chicken, ham, buff àn vegetables
+                  </p>
+                  <br>
+
+                  <a href="#" class="btn btn-danger">Order Now</a>
+
+              </div>
+
+      </div>
+
+
+    </div>
+    <div class="col-6">
+      <div class="p-3 border bg-light">
+      <div class="food-menu-img">
+                 <img src ="Images/menu-burger.jpg" alt="burger chicken"class="img-responsive img-curve">
+               </div>
+               <div class="food-menu-desc">
+                  <h4>Best burger</h4>
+                  <p class="food-price" >$4.00</p>
+                  <p class="food-detail">
+                      Burger with ham, pineapple and lots of cheese
+                  </p>
+                  <br>
+
+                  <a href="#" class="btn btn-danger">Order Now</a>
+
+              </div>
+      </div>
+    </div>
+    <div class="col-6">
+      <div class="p-3 border bg-light">
+      <div class="food-menu-img">
+                 <img src ="Images/menu-pizza.jpg" alt="Pizza chicken"class="img-responsive img-curve">
+               </div>
+               <div class="food-menu-desc">
+                  <h4>Smoky BBG Pizza</h4>
+                  <p class="food-price" >$6.00</p>
+                  <p class="food-detail">
+                      Made with Italia Sace, Chicken, and organice vegetable.
+                  </p>
+                  <br>
+
+                  <a href="#" class="btn btn-danger">Order Now</a>
+
+              </div>
+
+
+      </div>
+    </div>
+    <div class="col-6">
+      <div class="p-3 border bg-light">
+
+      <div class="food-menu-img">
+                 <img src ="Images/menu-momo.jpg" alt="Pizza chicken"class="img-responsive img-curve">
+               </div>
+               <div class="food-menu-desc">
+                  <h4>Sadeko Momo</h4>
+                  <p class="food-price" >$6.00</p>
+                  <p class="food-detail">
+                      Best spicy Momo for winter
+                  </p>
+                  <br>
+
+                  <a href="#" class="btn btn-danger">Order Now</a>
+
+              </div>
+
+      </div>
+    </div>
+  </div>
+</div>
+        
+
+
+
+
+     </div>
+  </div>
+</div>
+   <!-- end -->
+
+   <section class="social">
+ <div class="container text-center"> 
+ <ul>
+               <li>
+                  <a href="#"><img src="https://img.icons8.com/fluency/48/000000/facebook.png"/></a>  
+               </li> 
+               <li>
+                   <a  href="#" ><img src="https://img.icons8.com/color/48/000000/instagram-new.png"/></a>
+               </li> 
+               <li>
+                    <a  href="#" ><img src="https://img.icons8.com/color/48/000000/twitter--v1.png"/></a>
+               </li> 
+
+    </ul>
+</div>
+</section>
+
+<section class="end">
+ <div class="container text-center"> 
+ <p>All rights reserved. Designed By <a href="#" >CSE.TLU</a><p>  
+</div>
+</section> 
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  </body>
+</html>
