c4557c52ba3503240d2435e01d6b9cb662643c38
Taido0220 <91017652+Taido0220@users.noreply.github.com>
2021-09-26 22:16:46 -0700

    Add files via upload

---
+++ phpadmin/catagory.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+    <title>admin catagory</title>
+    <link rel ="stylesheet" href="css/stylead02.css">
+  </head>
+  <body>
+  <div class="menu text-center">
+             
+             <div class="row ">
+                 
+
+                 <ul>
+               <li>
+                  <a href="#" >Home</a>  
+               </li> 
+               <li>
+                  <a href="#" >Admin</a>  
+               </li> 
+               <li>
+                   <a  href="#" >Catagory</a>
+               </li> 
+               <li>
+                    <a  href="#" >Food</a>
+               </li> 
+               <li>
+                    <a  href="#" >Order</a>
+               </li> 
+               <li>
+                    <a  href="#" >Logout</a>
+               </li> 
+           </ul>
+
+                  </div>
+             </div>
+  </div>
+
+
+
+
+  <div class="main-content">
+
+
+
+
+ 
+             
+             <h1>Manage Catagory</h1>
+             <button type="button" class="btn btn-primary">Add Category</button>
+
+             <div class="row">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">S.N</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Featured</th>
+                    <th scope="col">Active</th>
+                    <th scope="col">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>Pizza</td>
+                    <td><img src="Images/menu-pizza.jpg" alt="pizza" class="img-fluid border-curve col-3"></td>
+                    <td>Yes</td>
+                    <td>Yes</td>
+                    <td><button type="button" class="btn btn-success text-dark">Update Catagory</button>
+                    <button type="button" class="btn btn-danger">Delete Catagory</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">2</th>
+                    <td>Burger</td>
+                    <td><img src="images/burger.jpg" alt="burger" class="img-fluid border-curve col-3"></td>
+                    <td>Yes</td>
+                    <td>Yes</td>
+                    <td><button type="button" class="btn btn-success text-dark">Update Catagory</button>
+                    <button type="button" class="btn btn-danger">Delete Catagory</button></td>
+                </tr>
+                <tr>
+                    <th scope="row">3</th>
+                    <td>MoMo</td>
+                    <td><img src="Images/menu-momo.jpg" alt="momo" class="img-fluid border-curve col-3"></td>
+                    <td>No</td>
+                    <td>Yes</td>
+                    <td><button type="button" class="btn btn-success text-dark">Update Catagory</button>
+                    <button type="button" class="btn btn-danger">Delete Catagory</button></td>
+                </tr>
+               
+            </tbody>
+        </table>
+            
+                 
+             </div>
+  </div>
+
+
+
+  <div class="footer">
+             
+             <div class="row">
+                 <div class="text-center "> <p>2020 All rights reserved. Designed By <a href="#" >CSE.TLU</a><p>  </div>
+             </div>
+  </div>
+  
+
+  
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+    -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+  </body>
+</html>
